c2740c25c41fa0d21cbb8fdfde9395bb0cd25473
Joshua Youden <Joshua.Youden@keyin.com>
2025-04-04 14:24:56 -0230

    Finished TreeNode and App, added code to controller

--- src/main/java/com/keyin/TreeController.java
+++ src/main/java/com/keyin/TreeController.java
@@ -1,4 +1,19 @@
 package com.keyin;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
 public class TreeController {
+    @Autowired
+    TreeService treeService;
+
+    @GetMapping("/enter-numbers")
+    public String enterNumbers() {}
+    
+    @PostMapping("/process-numbers")
+    public String processNumbers() {}
+
+    @PostMapping("/preview-trees")
+    public String previewTrees() {}
 }
--- src/main/java/com/keyin/TreeNode.java
+++ src/main/java/com/keyin/TreeNode.java
@@ -1,4 +1,59 @@
 package com.keyin;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
 public class TreeNode {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int value;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private TreeNode left;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private TreeNode right;
+
+    public TreeNode(int item) {
+        value = item;
+    }
+
+    public TreeNode() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
 }
