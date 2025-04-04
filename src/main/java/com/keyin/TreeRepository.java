9800a8042b746ba0ec35c4319f6fc433271e3da1
Joshua Youden <Joshua.Youden@keyin.com>
2025-04-04 12:19:25 -0230

    Added js files and code to java files

--- src/main/java/com/keyin/TreeRepository.java
+++ src/main/java/com/keyin/TreeRepository.java
@@ -1,4 +1,8 @@
 package com.keyin;
 
-public interface TreeRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TreeRepository extends CrudRepository<TreeRecord, Long> {
 }
